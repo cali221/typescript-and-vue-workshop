426007e4f3059819d714fbfd818c8a43afa0a4a7
Theresia Calista <theresiacalista57@gmail.com>
2025-04-05 21:34:59 +0700

    script setup on restaurants page

--- app/src/views/RestaurantsPage.vue
+++ app/src/views/RestaurantsPage.vue
@@ -1,99 +1,79 @@
-<script lang="ts">
-import { computed, defineComponent, onMounted, ref } from 'vue'
+<script setup lang="ts">
+import { computed, onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import NewRestaurantForm from '../components/NewRestaurantForm.vue'
 import RestaurantCard from '../components/RestaurantCard.vue'
 import SideMenu from '../components/SideMenu.vue'
 import type { Restaurant } from '@/types'
 
-export default defineComponent({
-  components: {
-    NewRestaurantForm,
-    RestaurantCard,
-    SideMenu,
+/**
+ * Restaurant Module
+ */
+const restaurantList = ref<Restaurant[]>([
+  {
+    id: '9f995ce4-d2fc-4d00-af1d-6cb1647c6bd3',
+    name: 'Quiche From a Rose',
+    address: '283 Thisisnota St.',
+    website: 'www.quichefromarose.com',
+    status: 'Want to Try',
   },
-  setup(){
-    /**
-     * Restaurant Module
-     */
-    const restaurantList = ref<Restaurant[]>([
-      {
-        id: '9f995ce4-d2fc-4d00-af1d-6cb1647c6bd3',
-        name: 'Quiche From a Rose',
-        address: '283 Thisisnota St.',
-        website: 'www.quichefromarose.com',
-        status: 'Want to Try',
-      },
-      {
-        id: 'ae62a3da-791b-4f44-99a1-4be1b0ec30b8',
-        name: 'Tamago Never Dies',
-        address: '529 Letsgofora Dr.',
-        website: 'www.tamagoneverdies.com',
-        status: 'Recommended',
-      },
-      {
-        id: '9b361dae-2d44-4499-9940-97e188d41a32',
-        name: 'Penne For Your Thoughts',
-        address: '870 Thisisa St.',
-        website: 'www.penneforyourthoughts.com',
-        status: 'Do Not Recommend',
-      },
-    ])
+  {
+    id: 'ae62a3da-791b-4f44-99a1-4be1b0ec30b8',
+    name: 'Tamago Never Dies',
+    address: '529 Letsgofora Dr.',
+    website: 'www.tamagoneverdies.com',
+    status: 'Recommended',
+  },
+  {
+    id: '9b361dae-2d44-4499-9940-97e188d41a32',
+    name: 'Penne For Your Thoughts',
+    address: '870 Thisisa St.',
+    website: 'www.penneforyourthoughts.com',
+    status: 'Do Not Recommend',
+  },
+])
 
-    const filteredRestaurantList = computed((): Restaurant[] => {
-      return restaurantList.value.filter((restaurant) => {
-        if (restaurant.name) {
-          return restaurant.name.toLowerCase().includes(filterText.value.toLowerCase())
-        } else {
-          return restaurantList.value
-        }
-      })
-    })
-
-    const addRestaurant = (payload: Restaurant) => {
-      restaurantList.value.push(payload)
-      hideForm()
+const filteredRestaurantList = computed((): Restaurant[] => {
+  return restaurantList.value.filter((restaurant) => {
+    if (restaurant.name) {
+      return restaurant.name.toLowerCase().includes(filterText.value.toLowerCase())
+    } else {
+      return restaurantList.value
     }
+  })
+})
 
-    const deleteRestaurant = (payload: Restaurant) => {
-      restaurantList.value = restaurantList.value.filter((restaurant) => {
-        return restaurant.id !== payload.id
-      })
-    }
+const addRestaurant = (payload: Restaurant) => {
+  restaurantList.value.push(payload)
+  hideForm()
+}
 
-    const numberOfRestaurants = computed((): number => {
-      return filteredRestaurantList.value.length
-    })
+const deleteRestaurant = (payload: Restaurant) => {
+  restaurantList.value = restaurantList.value.filter((restaurant) => {
+    return restaurant.id !== payload.id
+  })
+}
 
-    /**
-     * New Form Module
-     */
-    const showNewForm = ref(false) 
+const numberOfRestaurants = computed((): number => {
+  return filteredRestaurantList.value.length
+})
 
-    const filterText = ref('') 
+/**
+ * New Form Module
+ */
+const showNewForm = ref(false) 
 
-    const hideForm = () => {
-      showNewForm.value = false
-    }
+const filterText = ref('') 
 
-    onMounted (() => {
-      const route = useRoute()
+const hideForm = () => {
+  showNewForm.value = false
+}
 
-      if (route.query.new) {
-        showNewForm.value = true
-      }
-    })
+onMounted (() => {
+  const route = useRoute()
 
-    return{
-      addRestaurant,
-      deleteRestaurant,
-      filterText,
-      filteredRestaurantList,
-      hideForm,
-      numberOfRestaurants,
-      restaurantList,
-      showNewForm
-    }
+  if (route.query.new) {
+    showNewForm.value = true
   }
 })
 </script>
